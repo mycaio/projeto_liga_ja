0e3652f0fc1d0af3d83049cc835bb9a5fa9ea1cb
Mateus Prado <maateussp@gmail.com>
2026-02-26 10:34:20 -0300

    feat: separando o codigo e otimizando

---
+++ src/components/ButtonsOperations/style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  item: {
+    marginRight: 10,
+  },
+});
+
+export default style;
